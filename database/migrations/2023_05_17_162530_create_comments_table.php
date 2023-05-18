fdee529c4ab11f55073db02f64281821d76798e4
Hoaan <21211tt3903@mail.tdc.edu.vn>
2023-05-19 00:59:39 +0700

    add structure

--- database/migrations/2023_05_17_162530_create_comments_table.php
+++ database/migrations/2023_05_17_162530_create_comments_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('commments', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('commments');
+        Schema::dropIfExists('comments');
     }
 };
