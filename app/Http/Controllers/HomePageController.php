47e019e281f9bfa6b5ca7b70466eacfd05ef398d
Hoaan <21211tt3903@mail.tdc.edu.vn>
2023-05-23 02:16:12 +0700

    change

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -11,7 +11,7 @@ class HomePageController extends Controller
     public function index()
     {
         $animes = Anime::limit(3)->get();
-        $recentAnimes = Anime::orderBy('id', 'desc')->limit(8)->get();
+        $recentAnimes = Anime::orderBy('id', 'desc')->limit(4)->get();
         $bestAnimes = Anime::orderBy('imdb_rating', 'desc')->limit(4)->get();
         return view('pages.home.homepage', [
             'animes' => $animes,
